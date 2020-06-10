9358e3ab43cf0e43b7365db4a8f55a0b116657e4
Sebastian <seb.hollister@gatech.edu>
2020-06-10 02:39:12 -0400

    i aint trippin

--- gulpfile.js
+++ gulpfile.js
@@ -381,13 +381,3 @@ gulp.task('default', gulp.series('clean:dist', 'copy-assets', gulp.series('html'
 
 gulp.task('build', gulp.series('clean:dist', 'copy-assets', gulp.series('html', 'sass', 'sass-min', 'bootstrapjs', 'mrarejs')));
 
-
-var deploy      = require('gulp-gh-pages');
-
-/**
- * Push build to gh-pages
- */
-gulp.task('deploy', function () {
-  return gulp.src("./dist/**/*")
-    .pipe(deploy())
-});
